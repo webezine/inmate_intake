670e80eefc247e369ab30313c98979beb7c920f1
Daniel <webezine@hotmail.co.uk>
2020-04-20 14:22:13 +0100

    Created inmate module & imported inmate routing to app routing moduile

--- inmate-intake/src/app/app-routing.module.ts
+++ inmate-intake/src/app/app-routing.module.ts
@@ -5,7 +5,11 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: '', component: DashboardComponent },
-  { path: '*', component: DashboardComponent }
+  { path: '*', component: DashboardComponent },
+  {
+    path: 'inmates',
+    loadChildren: () => import('./inmate/inmate.module').then(m => m.InmateModule)
+  }
 ];
 
 @NgModule({
---
+++ inmate-intake/src/app/inmate/inmate.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgxMatDatetimePickerModule, NgxMatNativeDateModule, NgxMatTimepickerModule } from '@angular-material-components/datetime-picker';
+import { NgxMatMomentModule } from '@angular-material-components/moment-adapter';
+
+// Modules
+import { NgMaterialModule } from 'shared/ng-material.module';
+import { SharedModule } from 'shared/shared.module';
+import { InmateRoutingModule } from './inmate-routing.module';
+
+// Resolver
+import { InmateResolver } from './inmate.resolver';
+
+// Components
+import { EditComponent, LocationHistoryComponent } from './components/components;;
+
+@NgModule({
+  declarations: [EditComponent, LocationHistoryComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgMaterialModule,
+    SharedModule,
+    InmateRoutingModule,
+    NgxMatDatetimePickerModule,
+    NgxMatTimepickerModule,
+    NgxMatNativeDateModule,
+    NgxMatMomentModule
+  ],
+  providers: [InmateResolver]
+})
+export class InmateModule { }
