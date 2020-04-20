4f23e34e662f9f1b6c6729338e26dddde7dd5d88
Daniel <webezine@hotmail.co.uk>
2020-04-20 12:32:36 +0100

    Added login credential & user models to shared models

---
+++ inmate-intake/src/app/shared/models/user/user.ts
@@ -0,0 +1,9 @@
+export interface User {
+    id: number;
+    username: string;
+    firstNames: string;
+    lastName: string;
+    emailAddress: string;
+    isAdmin: boolean;
+    isActive: boolean;
+}
