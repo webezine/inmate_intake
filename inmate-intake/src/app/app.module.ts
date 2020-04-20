7e06edce2542b137c5eaa6522ed2dfecefd6b45a
Daniel <webezine@hotmail.co.uk>
2020-04-20 13:40:41 +0100

    Initial commit of dashboard component along with routing & module import.

--- inmate-intake/src/app/app.module.ts
+++ inmate-intake/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgMaterialModule }  from './shared/ng-material.module';
 import { SharedModule } from 'shared/shared.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
