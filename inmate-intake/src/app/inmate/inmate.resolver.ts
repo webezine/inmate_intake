a8e9c6345d2e2d61ba38bd775937e5ee59c9b387
Daniel <webezine@hotmail.co.uk>
2020-04-21 08:56:52 +0100

    Added mock data for inmates

--- inmate-intake/src/app/inmate/inmate.resolver.ts
+++ inmate-intake/src/app/inmate/inmate.resolver.ts
@@ -1,15 +1,23 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
 import { Resolve, ActivatedRouteSnapshot, Router } from '@angular/router';
-import { map } from 'rxjs/operators';
+import { map, takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 import { InmateService } from 'shared/services';
 
 @Injectable()
-export class InmateResolver implements Resolve<any> {
+export class InmateResolver implements Resolve<any>, OnDestroy {
   constructor(private inmateService: InmateService, private router: Router) { }
+  componentDestroyed$: Subject<boolean> = new Subject();
+
+  ngOnDestroy() {
+    this.componentDestroyed$.next(true)
+    this.componentDestroyed$.complete()
+  }
 
   resolve(route: ActivatedRouteSnapshot) {
     return this.inmateService.get(route.params.id).pipe(
+      takeUntil(this.componentDestroyed$),
       map(inmate => {
         if (inmate) {
           return inmate;
