2baa3cd7a979d44547350a639c92b0070c3efbbd
Daniel <webezine@hotmail.co.uk>
2020-04-21 23:06:40 +0100

    Updated routing for login page

--- inmate-intake/src/app/app-routing.module.ts
+++ inmate-intake/src/app/app-routing.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { AuthGuard } from 'core/guards/auth.guard';
+import { AuthGuard } from './core/guards';
+import { LoginComponent } from './core/components/login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
-  { path: '', component: DashboardComponent },
-  { path: '*', component: DashboardComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: DashboardComponent, canActivate: [AuthGuard] },
   {
     path: 'inmates',
     loadChildren: () => import('./inmate/inmate.module').then(m => m.InmateModule),
     canLoad: [AuthGuard]
-  }
+  },
+
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '*', redirectTo: 'home', pathMatch: 'full' }
 ];
 
 @NgModule({
