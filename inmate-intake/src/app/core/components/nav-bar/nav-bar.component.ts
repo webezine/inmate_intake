20b0af329800cf99e6cb3eb964b60ad0f0095b70
Daniel <webezine@hotmail.co.uk>
2020-04-21 15:40:14 +0100

    Implemented log out on nav bar

--- inmate-intake/src/app/core/components/nav-bar/nav-bar.component.ts
+++ inmate-intake/src/app/core/components/nav-bar/nav-bar.component.ts
@@ -1,19 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { AuthService } from 'core/services';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
-export class NavBarComponent implements OnInit {
+export class NavBarComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  constructor(
+    private authService: AuthService
+  ) { }
 
   logout() {
-
+    this.authService.logout();
   }
 
 }
