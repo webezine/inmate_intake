c0a538bb52c38a290c20d0095b3d3255f9669170
Daniel <webezine@hotmail.co.uk>
2020-04-21 16:34:57 +0100

    Created Auth Guard to protect routing for unauthorised access

---
+++ inmate-intake/src/app/core/guards/auth.guard.ts
@@ -0,0 +1,43 @@
+import { ActivatedRouteSnapshot, Route, Router, RouterStateSnapshot,
+  CanActivate, CanLoad, UrlSegment } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { Location } from '@angular/common';
+
+import { AuthService } from 'core/services';
+
+@Injectable({ providedIn: 'root' })
+export class AuthGuard implements CanActivate, CanLoad {
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private location: Location
+  ) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    return this.canAccess(state.url);
+  }
+
+  canLoad(route: Route, segments: UrlSegment[]): boolean {
+    return this.canAccess(this.location.path());
+  }
+
+  private canAccess(path: string) {
+    if (this.authService.isAuthenticated()) {
+      // token is in session storage > logged in so return true
+      return true;
+    }
+
+    if (path === '/unauthorised') {
+      this.navigateToLogin(path);
+      return false;
+    }
+
+    this.navigateToLogin(path);
+    return false;
+  }
+
+  private navigateToLogin(path: string) {
+    this.router.navigate(['/login'], { queryParams: { returnUrl: path } });
+    // ToDo  - implement login page
+  }
+}
